781225263dfae284a12f071d1fa195aeedbe7525
Omar1998-Claude <xxdestructionzxx@gmail.com>
2026-06-02 22:51:16 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "portfolio-v3";
+const CACHE = "portfolio-v4";
 const ASSETS = ["/", "/index.html", "/app.html", "/stock.html", "/manifest.json", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", e => {
